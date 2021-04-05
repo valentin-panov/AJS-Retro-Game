1f9babbb0fc8ae10802414ad055c822a20bc04c4
Valentin Panov <valentin.panov@me.com>
2021-04-05 22:45:42 +0300

    all is done except last two bunch of tests

--- src/js/__test__/GameController.test.js
+++ src/js/__test__/GameController.test.js
@@ -1,4 +1,14 @@
 /* eslint-disable no-undef */
+
+// ! tests
+/**
+ * 4) Не забудьте написать авто-тесты на функции/методы, которые лежат в
+ * основе п.1-4 (cursors)
+ * 5) Напишите авто-тест, с моком для метода load, который проверяет реакцию вашего
+ *  приложения на успешную и не успешную загрузку (при неуспешной загрузке
+ * должно выводиться сообщение через GamePlay - подумайте, как вы это будете тестировать).
+ */
+
 import GamePlay from '../GamePlay';
 import GameController from '../GameController';
 import GameStateService from '../GameStateService';
@@ -36,4 +46,10 @@ test('gameplay.showCellTooltip shouldnt trigger on mouse over unoccupied cell', 
   gameController.gamePlay.showCellTooltip = jest.fn();
   gameController.onCellEnter(2);
   expect(gameController.gamePlay.showCellTooltip).toBeCalledTimes(0);
+});
+
+test('gameplay.onCellLeave calls hideCellTooltip', () => {
+  gameController.gamePlay.hideCellTooltip = jest.fn();
+  gameController.onCellLeave(1);
+  expect(gameController.gamePlay.hideCellTooltip).toBeCalled();
 });
